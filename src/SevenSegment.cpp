22a6a8c778254f1e108a25005c8ac6d2ce7bdcbe
Hacks <yddrass@gmail.com>
2022-08-14 23:42:40 +0200

    format

--- src/SevenSegment.cpp
+++ src/SevenSegment.cpp
@@ -7,46 +7,54 @@
 
 SevenSegmentClass SevenSegment;
 
-void SevenSegmentClass::init() 
-{ 
-    pinMode(MAX7219_Data_IN, OUTPUT);
-    pinMode(MAX7219_Chip_Select, OUTPUT);
-    pinMode(MAX7219_Clock, OUTPUT);
-    digitalWrite(MAX7219_Clock, HIGH);
-    delay(200);
+void SevenSegmentClass::init()
+{
+  pinMode(MAX7219_Data_IN, OUTPUT);
+  pinMode(MAX7219_Chip_Select, OUTPUT);
+  pinMode(MAX7219_Clock, OUTPUT);
+  digitalWrite(MAX7219_Clock, HIGH);
+  delay(200);
 
-    //Setup of MAX7219 chip
-    shift(0x0f, 0x00); //display test register - test mode off
-    shift(0x0c, 0x01); //shutdown register - normal operation
-    shift(0x0b, 0x07); //scan limit register - display digits 0 thru 7
-    shift(0x0a, 0x07); //intensity register
-    shift(0x09, 0xff); //decode mode register - CodeB decode all digits
-    writeEmpty();
+  // Setup of MAX7219 chip
+  shift(0x0f, 0x00); // display test register - test mode off
+  shift(0x0c, 0x01); // shutdown register - normal operation
+  shift(0x0b, 0x07); // scan limit register - display digits 0 thru 7
+  shift(0x0a, 0x07); // intensity register
+  shift(0x09, 0xff); // decode mode register - CodeB decode all digits
+  writeEmpty();
 }
 
 void SevenSegmentClass::loop()
 {
-  if (millis() - _lastRefresh > (_refreshInterval * 1000)) {
+  if (millis() - _lastRefresh > (_refreshInterval * 1000))
+  {
     boolean wroteStats = false;
-    if(Hoymiles.getNumInverters() == 0){
+    if (Hoymiles.getNumInverters() == 0)
+    {
       writeEmpty();
       _lastShownPos = 0;
-    }else{
-      uint8_t next = _lastShownPos +1;
-      if(next >= Hoymiles.getNumInverters()){
+    }
+    else
+    {
+      uint8_t next = _lastShownPos + 1;
+      if (next >= Hoymiles.getNumInverters())
+      {
         next = 0;
       }
       auto inv = Hoymiles.getInverterByPos(next);
-      _lastShownPos=next;
-      for (uint8_t c = 0; c <= inv->Statistics()->getChannelCount(); c++) {
-        if(inv->Statistics()->hasChannelFieldValue(c,FLD_PAC)){
+      _lastShownPos = next;
+      for (uint8_t c = 0; c <= inv->Statistics()->getChannelCount(); c++)
+      {
+        if (inv->Statistics()->hasChannelFieldValue(c, FLD_PAC))
+        {
           uint8_t acPower = (uint8_t)inv->Statistics()->getChannelFieldValue(c, FLD_PAC) * 10;
-          write(next,acPower);
+          write(next, acPower);
           wroteStats = true;
           break;
         }
       }
-      if(!wroteStats || inv->Statistics()->getLastUpdate() < millis() - 60000){
+      if (!wroteStats || inv->Statistics()->getLastUpdate() < millis() - 60000)
+      {
         writeNa(next);
       }
     }
@@ -56,14 +64,14 @@ void SevenSegmentClass::loop()
 
 void SevenSegmentClass::writeEmpty()
 {
-  shift(0x08, DASH );
-  shift(0x07, DASH );
-  shift(0x06, DASH );
-  shift(0x05, DASH );
-  shift(0x04, DASH );
-  shift(0x03, DASH );
-  shift(0x02, DASH );
-  shift(0x01, DASH );
+  shift(0x08, DASH);
+  shift(0x07, DASH);
+  shift(0x06, DASH);
+  shift(0x05, DASH);
+  shift(0x04, DASH);
+  shift(0x03, DASH);
+  shift(0x02, DASH);
+  shift(0x01, DASH);
 }
 
 void SevenSegmentClass::shift(byte send_to_address, byte send_this_data)
@@ -74,37 +82,37 @@ void SevenSegmentClass::shift(byte send_to_address, byte send_this_data)
   digitalWrite(MAX7219_Chip_Select, HIGH);
 }
 
-void SevenSegmentClass::writeNa(int inverter){
+void SevenSegmentClass::writeNa(int inverter)
+{
   writeInverter(inverter);
-  shift(0x05,BLANK);
-  shift(0x04,DASH);
-  shift(0x03,DASH);
-  shift(0x02,DASH);
-  shift(0x01,DASH);
+  shift(0x05, BLANK);
+  shift(0x04, DASH);
+  shift(0x03, DASH);
+  shift(0x02, DASH);
+  shift(0x01, DASH);
 }
 
-void SevenSegmentClass::writeInverter(int inverter){
+void SevenSegmentClass::writeInverter(int inverter)
+{
   inverter++; // start at 1
-  //inverter
-  shift(0x08,inverter / 10);
-  shift(0x07,inverter % 10);
-  shift(0x06,DASH); // blinking dash as activity indicator
+  // inverter
+  shift(0x08, inverter / 10);
+  shift(0x07, inverter % 10);
+  shift(0x06, DASH); // blinking dash as activity indicator
 }
 
 void SevenSegmentClass::write(int inverter, int acPower)
 {
   writeInverter(inverter);
-  //power
-  shift(0x05,acPower / 10000 == 0 ? BLANK : acPower / 10000);
+  // power
+  shift(0x05, acPower / 10000 == 0 ? BLANK : acPower / 10000);
   acPower = acPower % 10000;
-  shift(0x04,acPower / 1000 == 0 ? BLANK : acPower / 1000);
+  shift(0x04, acPower / 1000 == 0 ? BLANK : acPower / 1000);
   acPower = acPower % 1000;
-  shift(0x03,acPower / 100 == 0 ? BLANK : acPower / 100);
+  shift(0x03, acPower / 100 == 0 ? BLANK : acPower / 100);
   acPower = acPower % 100;
-  //decimal
-  shift(0x02,(acPower / 10) + 128 );
+  // decimal
+  shift(0x02, (acPower / 10) + 128);
   acPower = acPower % 10;
-  shift(0x01,acPower);
+  shift(0x01, acPower);
 }
-
-    
--- src/SevenSegment.h
+++ src/SevenSegment.h
@@ -3,25 +3,26 @@
 #define MAX7219_Data_IN 26
 #endif
 #ifndef MAX7219_Chip_Select
-#define MAX7219_Chip_Select  27
+#define MAX7219_Chip_Select 27
 #endif
 #ifndef MAX7219_Clock
 #define MAX7219_Clock 25
 #endif
 
-class SevenSegmentClass {
+class SevenSegmentClass
+{
+public:
+  void init();
+  void loop();
 
-  public:
-    void init();
-    void loop();
-  private:
-    void write(int inverter, int acPower);
-    void writeEmpty();
-    void shift(byte send_to_address, byte send_this_data);
-    void writeNa(int inverter);
-    void writeInverter(int inverter);
-    uint32_t _refreshInterval = 10;
-    uint32_t _lastRefresh = 0;
-    uint8_t _lastShownPos = 0;
+private:
+  void write(int inverter, int acPower);
+  void writeEmpty();
+  void shift(byte send_to_address, byte send_this_data);
+  void writeNa(int inverter);
+  void writeInverter(int inverter);
+  uint32_t _refreshInterval = 10;
+  uint32_t _lastRefresh = 0;
+  uint8_t _lastShownPos = 0;
 };
 extern SevenSegmentClass SevenSegment;
